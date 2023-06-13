10c6d8006a6cb23252f0f9dcd4d05216be49c586
Alin <alin.dulceanu@gmail.com>
2023-06-13 20:11:51 +0300

    Am mai gasit niste probleme rezolvate anterior

--- Probleme Geeks4Geeks/g4g_least_prime.py
+++ Probleme Geeks4Geeks/g4g_least_prime.py
@@ -1,3 +1,9 @@
+'''Given a number N, find the least prime factors for all numbers from 1 to N.  The least prime factor of an integer X is the smallest prime number that divides it.
+Note :
+
+1 needs to be printed for 1.
+You need to return an array/vector/list of size N+1 and need to use 1-based indexing to store the answer for each number.'''
+
 class Solution:
     def leastPrimeFactor (n):
         primes = [0 for i in range(n + 1)]
