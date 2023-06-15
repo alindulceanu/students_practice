299a6ae7fccd1962e1cbf3d4df07e2e39c23e729
Alin <alin.dulceanu@gmail.com>
2023-06-15 13:49:26 +0300

    Am creat o functie iterativa pentru generarea numarului Fibonacci si am pus un time table pentru n = 10000 si n = 100000

--- Dinamic Programming/dinamic_programming.py
+++ Dinamic Programming/dinamic_programming.py
@@ -1,3 +1,9 @@
+import time
+import sys
+
+sys.setrecursionlimit(1000000)
+sys.set_int_max_str_digits(99999999)
+
 class dp:
     def fib(self, n, memo = {}):
         if n == 0 or n == 1:
@@ -6,7 +12,7 @@ class dp:
         if n in memo:
             return memo[n]
         
-        memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
+        memo[n] = self.fib(n - 1, memo) + self.fib(n - 2, memo)
         return memo[n]
 
     def fibBottomUp(self, n):
@@ -22,8 +28,32 @@ class dp:
             bottomUp[i] = bottomUp[i - 1] + bottomUp[i - 2]
 
         return bottomUp[n]
+    
+    def fib_iter(self, n):
+        current = 1
+        last = 0
 
+        for i in range(n - 1):
+            x = current
+            current += last
+            last = x
 
+        return current
+
+start = time.time()
 ob = dp()
-print(ob.fibBottomUp(9999))
+print(ob.fib_iter(100000), "\n")
+print(str(time.time() - start) + "s")
 
+
+#TIMETABLE
+
+#fib(10000) - 0.008335113525390625s
+#fibBottomUp(10000) - 0.006401538848876953s
+#fib_iter(10000) - 0.004000186920166016s
+
+#fib(100000) - 0.18917226791381836s
+#fibBottomUp(100000) - 0.2032630443572998s
+#fib_iter(100000) - 0.08015084266662598s
+
+#fib_iter Best time
